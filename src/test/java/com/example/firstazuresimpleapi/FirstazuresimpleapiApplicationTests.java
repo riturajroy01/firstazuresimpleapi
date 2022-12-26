e8482013f1684a2528a41625c2fa618e01ff3e7f
riturajroy01 <web12345678>
2022-12-26 22:34:44 +0100

    Initial commit

---
+++ src/test/java/com/example/firstazuresimpleapi/FirstazuresimpleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstazuresimpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstazuresimpleapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
